45ebab058c7cd2ec953bda3538feb209e66a5e73
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-07 18:52:30 +0600

    working on wallet generation

--- server/app/Controllers/WalletController.js
+++ server/app/Controllers/WalletController.js
@@ -6,16 +6,26 @@ const asyncHandler = require('express-async-handler')
 
 const generateKeys = asyncHandler(
     async (req, res) => {
+        const { nid } = req.body
         //Key pair should be generated based on NID 
         const key = ec.genKeyPair();
         const publicKey = key.getPublic('hex');
         const privateKey = key.getPrivate('hex');
 
-        let keyPair = {
+        let voterInfo = {
             'Public key' : publicKey,
-            'Private key' : privateKey
+            'Private key' : privateKey,
+            'nid' : nid
         } 
         res.status(200).json(keyPair)
+        const response = await fetch(`${network[i]}/api/chainList`);
+        const data = await response.json();
+
+        if (response.status == 200){
+            //
+        }
+
+        res.status(200).json();
     }
 )
 
