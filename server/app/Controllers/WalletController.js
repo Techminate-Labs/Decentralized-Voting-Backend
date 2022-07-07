fd3a3b8efb25bb9a44bd49ee3eee7e391a0d8687
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-07 19:08:03 +0600

    working on voter wallet

--- server/app/Controllers/WalletController.js
+++ server/app/Controllers/WalletController.js
@@ -4,25 +4,35 @@ const ec = new EC('secp256k1');
 
 const asyncHandler = require('express-async-handler')
 
-const generateKeys = asyncHandler(
-    async (req, res) => {
-        const { nid } = req.body
+function generateWallet(nid) {
         //Key pair should be generated based on NID 
         const key = ec.genKeyPair();
         const publicKey = key.getPublic('hex');
         const privateKey = key.getPrivate('hex');
 
-        let voterInfo = {
+        const voterInfo = {
             'Public key' : publicKey,
             'Private key' : privateKey,
             'nid' : nid
-        } 
-        res.status(200).json(keyPair)
-        const response = await fetch(`${network[i]}/api/chainList`);
+        }
+
+        return voterInfo;
+    }
+
+const voterWallet = asyncHandler(
+    async (req, res) => {
+        const { nid } = req.body
+        const url = 'http://localhost:8080/citizen'
+        const response = await fetch(url);
         const data = await response.json();
 
         if (response.status == 200){
-            //
+            data.map((citizen)=>{
+                if(citizen.id === nid){
+                    const voterInfo =  generateWallet(nid)
+                    res.status(200).json(generateWallet(nid))
+                }
+            })
         }
 
         res.status(200).json();
