6c6698a2156a57fb78dab220df0bee05341e8670
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-13 00:45:58 +0600

    cors added

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./app/Middleware/ErrorMiddleware')
-const connectDB = require('./config/db')
+const express = require('express');
+const colors = require('colors');
+const cors = require("cors");
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./app/Middleware/ErrorMiddleware');
+const connectDB = require('./config/db');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
+// Whitelist your url for cors (This is a sample url only you can add your own url.)
+const whitelist = ["http://localhost:3000", "http://localhost:5000"];
 
-app.use(express.json())
+// Check if the origin is trying to access our api.
+// Check if it's one of the whitelist website.
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+// use cors and add options
+app.use(cors(corsOptions));
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', require('./routes/api'))
